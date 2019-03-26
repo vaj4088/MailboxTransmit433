24d3cc99e64fd9fa77333b9b898806acc75927bb
vaj4088 <ianshef@yahoo.com>
2019-03-26 14:58:38 -0500

    More files as part of project creation process.

---
+++ MailboxTransmit433/MailboxTransmit433.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _MailboxTransmit433_H_
+#define _MailboxTransmit433_H_
+#include "Arduino.h"
+//add your includes for the project MailboxTransmit433 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project MailboxTransmit433 here
+
+
+
+
+//Do not add code below this line
+#endif /* _MailboxTransmit433_H_ */
